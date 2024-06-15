403ff4bd00390e1fcf9642de5aa69458fd3839fe
CarolinaLD <carolinalopezdelgado20@gmail.com>
2024-06-15 16:48:32 -0300

    Formulario Listado Tipo Paquete Listo

--- Views/TipoPaquete/FrmListadoTipoPaquete.Designer.cs
+++ Views/TipoPaquete/FrmListadoTipoPaquete.Designer.cs
@@ -28,20 +28,172 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.TipoPaquetesGrd = new System.Windows.Forms.DataGridView();
+            this.CodigoCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NombreCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DescripcionCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DuracionCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NivelCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.NombreTxt = new System.Windows.Forms.TextBox();
+            this.NombreChk = new System.Windows.Forms.CheckBox();
+            this.FiltroBtn = new System.Windows.Forms.Button();
+            this.ExportarBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.TipoPaquetesGrd)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // TipoPaquetesGrd
+            // 
+            this.TipoPaquetesGrd.AllowUserToAddRows = false;
+            this.TipoPaquetesGrd.AllowUserToDeleteRows = false;
+            this.TipoPaquetesGrd.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.TipoPaquetesGrd.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.CodigoCol,
+            this.NombreCol,
+            this.DescripcionCol,
+            this.DuracionCol,
+            this.NivelCol});
+            this.TipoPaquetesGrd.Location = new System.Drawing.Point(25, 57);
+            this.TipoPaquetesGrd.Margin = new System.Windows.Forms.Padding(2);
+            this.TipoPaquetesGrd.MultiSelect = false;
+            this.TipoPaquetesGrd.Name = "TipoPaquetesGrd";
+            this.TipoPaquetesGrd.ReadOnly = true;
+            this.TipoPaquetesGrd.RowTemplate.Height = 24;
+            this.TipoPaquetesGrd.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.TipoPaquetesGrd.ShowEditingIcon = false;
+            this.TipoPaquetesGrd.Size = new System.Drawing.Size(640, 226);
+            this.TipoPaquetesGrd.TabIndex = 12;
+            this.TipoPaquetesGrd.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.TipoPaquetesGrd_CellContentClick);
+            this.TipoPaquetesGrd.ColumnHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.TipoPaquetesGrd_ColumnHeaderMouseClick);
+            this.TipoPaquetesGrd.DataBindingComplete += new System.Windows.Forms.DataGridViewBindingCompleteEventHandler(this.TipoPaquetesGrd_DataBindingComplete);
+            this.TipoPaquetesGrd.DoubleClick += new System.EventHandler(this.TipoPaquetesGrd_DoubleClick);
+            // 
+            // CodigoCol
+            // 
+            this.CodigoCol.DataPropertyName = "Codigo";
+            this.CodigoCol.HeaderText = "Codigo";
+            this.CodigoCol.Name = "CodigoCol";
+            this.CodigoCol.ReadOnly = true;
+            // 
+            // NombreCol
+            // 
+            this.NombreCol.DataPropertyName = "Nombres";
+            this.NombreCol.HeaderText = "Nombres";
+            this.NombreCol.Name = "NombreCol";
+            this.NombreCol.ReadOnly = true;
+            this.NombreCol.Width = 150;
+            // 
+            // DescripcionCol
+            // 
+            this.DescripcionCol.DataPropertyName = "Descripcion";
+            this.DescripcionCol.HeaderText = "Descripcion";
+            this.DescripcionCol.Name = "DescripcionCol";
+            this.DescripcionCol.ReadOnly = true;
+            this.DescripcionCol.Width = 150;
+            // 
+            // DuracionCol
+            // 
+            this.DuracionCol.DataPropertyName = "Duracion";
+            this.DuracionCol.HeaderText = "Duracion";
+            this.DuracionCol.Name = "DuracionCol";
+            this.DuracionCol.ReadOnly = true;
+            // 
+            // NivelCol
+            // 
+            this.NivelCol.DataPropertyName = "Nivel";
+            this.NivelCol.HeaderText = "Nivel";
+            this.NivelCol.Name = "NivelCol";
+            this.NivelCol.ReadOnly = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.NombreTxt);
+            this.groupBox1.Controls.Add(this.NombreChk);
+            this.groupBox1.Location = new System.Drawing.Point(11, 11);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(227, 42);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            // 
+            // NombreTxt
+            // 
+            this.NombreTxt.Enabled = false;
+            this.NombreTxt.Location = new System.Drawing.Point(69, 15);
+            this.NombreTxt.Margin = new System.Windows.Forms.Padding(2);
+            this.NombreTxt.Name = "NombreTxt";
+            this.NombreTxt.Size = new System.Drawing.Size(146, 20);
+            this.NombreTxt.TabIndex = 1;
+            // 
+            // NombreChk
+            // 
+            this.NombreChk.AutoSize = true;
+            this.NombreChk.Location = new System.Drawing.Point(4, 17);
+            this.NombreChk.Margin = new System.Windows.Forms.Padding(2);
+            this.NombreChk.Name = "NombreChk";
+            this.NombreChk.Size = new System.Drawing.Size(63, 17);
+            this.NombreChk.TabIndex = 0;
+            this.NombreChk.Text = "Nombre";
+            this.NombreChk.UseVisualStyleBackColor = true;
+            this.NombreChk.CheckedChanged += new System.EventHandler(this.TipoPaqueteChk_CheckedChanged);
+            // 
+            // FiltroBtn
+            // 
+            this.FiltroBtn.Location = new System.Drawing.Point(570, 21);
+            this.FiltroBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.FiltroBtn.Name = "FiltroBtn";
+            this.FiltroBtn.Size = new System.Drawing.Size(76, 32);
+            this.FiltroBtn.TabIndex = 15;
+            this.FiltroBtn.Text = "Filtrar";
+            this.FiltroBtn.UseVisualStyleBackColor = true;
+            this.FiltroBtn.Click += new System.EventHandler(this.FiltroBtn_Click);
+            // 
+            // ExportarBtn
+            // 
+            this.ExportarBtn.Location = new System.Drawing.Point(562, 293);
+            this.ExportarBtn.Name = "ExportarBtn";
+            this.ExportarBtn.Size = new System.Drawing.Size(84, 26);
+            this.ExportarBtn.TabIndex = 16;
+            this.ExportarBtn.Text = "Exportar";
+            this.ExportarBtn.UseVisualStyleBackColor = true;
+            this.ExportarBtn.Click += new System.EventHandler(this.ExportarBtn_Click);
             // 
             // FrmListadoTipoPaquete
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(685, 331);
+            this.Controls.Add(this.ExportarBtn);
+            this.Controls.Add(this.FiltroBtn);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.TipoPaquetesGrd);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "FrmListadoTipoPaquete";
-            this.Text = "FrmListadoTipoPaquete";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Listado Tipo Paquete";
             this.Load += new System.EventHandler(this.FrmListadoTipoPaquete_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.TipoPaquetesGrd)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView TipoPaquetesGrd;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CodigoCol;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NombreCol;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DescripcionCol;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DuracionCol;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NivelCol;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox NombreTxt;
+        private System.Windows.Forms.CheckBox NombreChk;
+        private System.Windows.Forms.Button FiltroBtn;
+        private System.Windows.Forms.Button ExportarBtn;
     }
 }
--- Views/TipoPaquete/FrmListadoTipoPaquete.cs
+++ Views/TipoPaquete/FrmListadoTipoPaquete.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -20,7 +21,181 @@ namespace TurApp.Views
 
         private void FrmListadoTipoPaquete_Load(object sender, EventArgs e)
         {
+            this.TipoPaquetesGrd.AutoGenerateColumns = false;
+            // this.TipoPaquetesGrd.DataSource = TipoPaquete.FindAllStatic(null, (p1, p2) => (p1.Nombre).CompareTo(p2.Nombre));
+            var TipoPaquetes = TipoPaquete .FindAllStatic(null, (p1, p2) => p1.Nombre.CompareTo(p2.Nombre));
+            var TipoPaquetesBindingList = new BindingList<TipoPaquete>(TipoPaquetes);
+            var TipoPaquetesBindingSource = new BindingSource(TipoPaquetesBindingList, null);
+            this.TipoPaquetesGrd.DataSource = TipoPaquetesBindingSource;
+
+            this.ExportarBtn.Enabled = true;
+
+            this.TipoPaquetesGrd.ColumnHeaderMouseClick += new DataGridViewCellMouseEventHandler(TipoPaquetesGrd_ColumnHeaderMouseClick);
+        }
+
+        private void TipoPaqueteChk_CheckedChanged(object sender, EventArgs e)
+        {
+            this.NombreTxt.Enabled = this.NombreChk.Checked;
 
         }
+
+        private void FiltroBtn_Click(object sender, EventArgs e)
+        {
+            string criterio = null;
+
+            if (this.NombreChk.Checked && this.NombreChk != null)
+            {
+                criterio = String.Format("Nombre = '{0}'", NombreTxt.Text);
+            }
+
+            this.TipoPaquetesGrd.DataSource = TipoPaquete.FindAllStatic(criterio, (p1, p2) => (p1.Nombre).CompareTo(p2.Nombre));
+
+        }
+        private void TipoPaquetesGrd_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void TipoPaquetesGrd_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
+        {
+            this.TipoPaquetesGrd.DataBindingComplete -= TipoPaquetesGrd_DataBindingComplete;
+
+            try
+            {
+                for (int i = 0; i < this.TipoPaquetesGrd.Rows.Count; ++i)
+                {
+                    DataGridViewRow item = this.TipoPaquetesGrd.Rows[i];
+                    item.Cells[1].Value = (item.DataBoundItem as FormaPago).Forma;
+                }
+            }
+            finally
+            {
+                this.TipoPaquetesGrd.DataBindingComplete += TipoPaquetesGrd_DataBindingComplete;
+            }
+
+        }
+
+        private void TipoPaquetesGrd_DoubleClick(object sender, EventArgs e)
+        {
+            FrmTipoPaqueteAM frmpac = new FrmTipoPaqueteAM();
+            TipoPaquete pac = (this.TipoPaquetesGrd.SelectedRows[0].DataBoundItem as TipoPaquete);
+            frmpac.ShowModificarTipoPaquete(pac);
+        }
+
+        private void ExportarBtn_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog sfd = new SaveFileDialog())
+            {
+                sfd.Filter = "CSV files (*.csv)|*.csv|All files (*.*)|*.*";
+                sfd.FilterIndex = 1;
+                sfd.RestoreDirectory = true;
+
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    ExportToCsv(TipoPaquetesGrd, sfd.FileName);
+                }
+            }
+        }
+
+        private void ExportToCsv(DataGridView dgv, string filePath)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(filePath))
+                {
+                    // Escribir encabezados
+                    for (int i = 0; i < dgv.Columns.Count; i++)
+                    {
+                        sw.Write(dgv.Columns[i].HeaderText);
+                        if (i < dgv.Columns.Count - 1)
+                        {
+                            sw.Write(",");
+                        }
+                    }
+                    sw.WriteLine();
+
+                    // Escribir datos
+                    for (int i = 0; i < dgv.Rows.Count; i++)
+                    {
+                        for (int j = 0; j < dgv.Columns.Count; j++)
+                        {
+                            sw.Write(dgv.Rows[i].Cells[j].Value);
+                            if (j < dgv.Columns.Count - 1)
+                            {
+                                sw.Write(",");
+                            }
+                        }
+                        sw.WriteLine();
+                    }
+                }
+                MessageBox.Show("Datos exportados con éxito.", "Exportación completada", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al exportar datos: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void TipoPaquetesGrd_ColumnHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            string sortOrderGrid = "";
+
+            if (TipoPaquetesGrd.Tag != null)
+                sortOrderGrid = TipoPaquetesGrd.Tag.ToString();
+
+            DataGridViewColumn newColumn = TipoPaquetesGrd.Columns[e.ColumnIndex];
+
+            // Determinar la dirección de orden actual
+            ListSortDirection direction;
+            if (sortOrderGrid.StartsWith("-") && sortOrderGrid.Substring(1) == newColumn.Name)
+                direction = ListSortDirection.Ascending;
+            else
+                direction = sortOrderGrid.StartsWith("-") ? ListSortDirection.Descending : ListSortDirection.Ascending;
+
+            // Obtener los tipo paquetes desde el DataSource
+            var bindingSource = TipoPaquetesGrd.DataSource as BindingSource;
+            if (bindingSource == null)
+            {
+                MessageBox.Show("DataSource no es un BindingSource.");
+                return;
+            }
+
+            var TipoPaquete = bindingSource.List.Cast<TipoPaquete>().ToList();
+
+            // Ordenar los tipo paquetes según la columna seleccionada
+            switch (newColumn.Name)
+            {
+                case "CodigoCol":
+                    TipoPaquete.Sort((t1, t2) => direction == ListSortDirection.Ascending ? t1.Codigo.CompareTo(t2.Codigo) : t2.Codigo.CompareTo(t1.Codigo));
+                    break;
+                case "NombreCol":
+                    TipoPaquete.Sort((t1, t2) => direction == ListSortDirection.Ascending ? t1.Nombre.CompareTo(t2.Nombre) : t2.Nombre.CompareTo(t1.Nombre));
+                    break;
+
+                case "DescripcionCol":
+                    TipoPaquete.Sort((t1, t2) => direction == ListSortDirection.Ascending ? t1.Descripcion.CompareTo(t2.Descripcion) : t2.Descripcion.CompareTo(t1.Descripcion));
+                    break;
+
+                case "DuracionCol":
+                    TipoPaquete.Sort((t1, t2) => direction == ListSortDirection.Ascending ? t1.Duracion.CompareTo(t2.Duracion) : t2.Duracion.CompareTo(t1.Duracion));
+                    break;
+
+                case "NivelCol":
+                    TipoPaquete.Sort((t1, t2) => direction == ListSortDirection.Ascending ? t1.Nivel.CompareTo(t2.Nivel) : t2.Nivel.CompareTo(t1.Nivel));
+                    break;
+            }
+
+            // Cambiar la dirección de orden para la próxima vez
+            direction = (direction == ListSortDirection.Ascending) ? ListSortDirection.Descending : ListSortDirection.Ascending;
+
+            // Actualizar la etiqueta de dirección de orden
+            TipoPaquetesGrd.Tag = direction == ListSortDirection.Ascending ? "" : "-" + newColumn.Name;
+
+            // Asignar los datos ordenados al DataSource
+            bindingSource.DataSource = new BindingList<TipoPaquete>(TipoPaquete);
+
+            // Mostrar la dirección de orden en el encabezado de la columna
+            newColumn.HeaderCell.SortGlyphDirection = direction == ListSortDirection.Ascending ? SortOrder.Ascending : SortOrder.Descending;
+        }   
     }
 }
