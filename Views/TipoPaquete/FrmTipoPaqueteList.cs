cb6bf2a86efe7b4836f9170d53abbb78bcc71537
CarolinaLD <carolinalopezdelgado20@gmail.com>
2024-06-15 13:12:06 -0300

    Formulario de tipo paquete list

--- Views/TipoPaquete/FrmTipoPaqueteList.cs
+++ Views/TipoPaquete/FrmTipoPaqueteList.cs
@@ -12,14 +12,90 @@ namespace TurApp.Views
 {
     public partial class FrmTipoPaqueteList : FormBase
     {
+        private string _criterio = null;
+        private List<TipoPaquete> _listado;
+
         public FrmTipoPaqueteList()
         {
             InitializeComponent();
         }
 
+        public void ShowListado(List<TipoPaquete> listado, FormBase Invoker, string criterio)
+        {
+            this.InvokerForm = Invoker;
+            _listado = listado;
+            _criterio = criterio;
+            this.TipoPaquetesGrd.AutoGenerateColumns = false;
+            var bindingList = new BindingList<TipoPaquete>(listado);
+            var source = new BindingSource(bindingList, null);
+            this.TipoPaquetesGrd.DataSource = source;
+            InvokerForm.Close();
+            this.MdiParent = MainView.Instance;
+            this.Show();
+        }
+
         private void FrmTipoPaqueteList_Load(object sender, EventArgs e)
         {
 
+        }
+        private void CerrarBtn_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void TipoPaquetesGrd_DoubleClick(object sender, EventArgs e)
+        {
+            if (this.TipoPaquetesGrd.SelectedRows.Count > 0)
+            {
+                MainView.Instance.Cursor = Cursors.WaitCursor;
+                FrmTipoPaqueteAM frm = new FrmTipoPaqueteAM();
+                frm.DoCompleteOperationForm += new FormEvent(frm_DoCompleteOperationForm);
+                frm.ShowModificarTipoPaquete(this, (this.TipoPaquetesGrd.SelectedRows[0].DataBoundItem as TipoPaquete));
+            }
+
+        }
+
+        void frm_DoCompleteOperationForm(object Sender, EventArgDom ev)
+        {
+            this.Cursor = Cursors.Default;
+            if (ev.Status == TipoOperacionStatus.stOK)
+            {
+                var selAnt = TipoPaquetesGrd.SelectedRows[0].Index;
+                this.TipoPaquetesGrd.DataSource = TipoPaquete.FindAllStatic(_criterio, (e1, e2) => e1.Codigo.CompareTo(e2.Codigo));
+                TipoPaquetesGrd.Rows[selAnt].Selected = true;
+                MessageBox.Show("Tipo Paquete actualizado", "Exito...", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void TipoPaquetesGrd_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
+        {
+            for (int i = 0; i < this.TipoPaquetesGrd.Rows.Count; ++i)
+            {
+                foreach (DataGridViewRow rw in this.TipoPaquetesGrd.Rows)
+                {
+                    rw.Cells[0].Value = (rw.DataBoundItem as TipoPaquete).Codigo;
+                }
+
+                foreach (DataGridViewRow rw in this.TipoPaquetesGrd.Rows)
+                {
+                    rw.Cells[1].Value = (rw.DataBoundItem as TipoPaquete).Nombre;
+                }
+
+                foreach (DataGridViewRow rw in this.TipoPaquetesGrd.Rows)
+                {
+                    rw.Cells[2].Value = (rw.DataBoundItem as TipoPaquete).Descripcion;
+                }
+
+                foreach (DataGridViewRow rw in this.TipoPaquetesGrd.Rows)
+                {
+                    rw.Cells[3].Value = (rw.DataBoundItem as TipoPaquete).Duracion;
+                }
+
+                foreach (DataGridViewRow rw in this.TipoPaquetesGrd.Rows)
+                {
+                    rw.Cells[4].Value = (rw.DataBoundItem as TipoPaquete).Nivel;
+                }
+            }
         }
     }
 }
