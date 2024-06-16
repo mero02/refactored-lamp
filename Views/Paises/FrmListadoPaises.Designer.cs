0b519ea16f8e4b7dd82a42f80066e620728e91e2
0uros <matyporello@gmail.com>
2024-06-16 03:13:49 -0300

    "Implementación parcial de ListadoPaises"

---
+++ Views/Paises/FrmListadoPaises.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace TurApp.Views
+{
+    partial class FrmListadoPaises
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ExportarBtn = new System.Windows.Forms.Button();
+            this.PaisesGrd = new System.Windows.Forms.DataGridView();
+            this.CodigoCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NombreCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CantidadCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.PaisesGrd)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ExportarBtn
+            // 
+            this.ExportarBtn.Location = new System.Drawing.Point(745, 446);
+            this.ExportarBtn.Name = "ExportarBtn";
+            this.ExportarBtn.Size = new System.Drawing.Size(84, 26);
+            this.ExportarBtn.TabIndex = 12;
+            this.ExportarBtn.Text = "Exportar";
+            this.ExportarBtn.UseVisualStyleBackColor = true;
+            this.ExportarBtn.Click += new System.EventHandler(this.ExportarBtn_Click);
+            // 
+            // PaisesGrd
+            // 
+            this.PaisesGrd.AllowUserToAddRows = false;
+            this.PaisesGrd.AllowUserToDeleteRows = false;
+            this.PaisesGrd.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.PaisesGrd.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.CodigoCol,
+            this.NombreCol,
+            this.CantidadCol});
+            this.PaisesGrd.Location = new System.Drawing.Point(11, 27);
+            this.PaisesGrd.Margin = new System.Windows.Forms.Padding(2);
+            this.PaisesGrd.MultiSelect = false;
+            this.PaisesGrd.Name = "PaisesGrd";
+            this.PaisesGrd.ReadOnly = true;
+            this.PaisesGrd.RowTemplate.Height = 24;
+            this.PaisesGrd.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.PaisesGrd.ShowEditingIcon = false;
+            this.PaisesGrd.Size = new System.Drawing.Size(826, 414);
+            this.PaisesGrd.TabIndex = 11;
+            this.PaisesGrd.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.PaisesGrd_CellContentClick);
+            this.PaisesGrd.ColumnHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.PaisesGrd_ColumnHeaderMouseClick);
+            this.PaisesGrd.DataBindingComplete += new System.Windows.Forms.DataGridViewBindingCompleteEventHandler(this.PaisesGrd_DataBindingComplete);
+            // 
+            // CodigoCol
+            // 
+            this.CodigoCol.DataPropertyName = "Codigo";
+            this.CodigoCol.HeaderText = "Codigo";
+            this.CodigoCol.Name = "CodigoCol";
+            this.CodigoCol.ReadOnly = true;
+            // 
+            // NombreCol
+            // 
+            this.NombreCol.DataPropertyName = "Nombre";
+            this.NombreCol.HeaderText = "Pais";
+            this.NombreCol.Name = "NombreCol";
+            this.NombreCol.ReadOnly = true;
+            this.NombreCol.Width = 150;
+            // 
+            // CantidadCol
+            // 
+            this.CantidadCol.DataPropertyName = "Cantidad";
+            this.CantidadCol.HeaderText = "Descripcion";
+            this.CantidadCol.Name = "CantidadCol";
+            this.CantidadCol.ReadOnly = true;
+            this.CantidadCol.Width = 500;
+            // 
+            // FrmListadoPaises
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(860, 493);
+            this.Controls.Add(this.ExportarBtn);
+            this.Controls.Add(this.PaisesGrd);
+            this.Name = "FrmListadoPaises";
+            this.Text = "FrmListadoPaises";
+            this.Load += new System.EventHandler(this.FrmListadoPaises_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PaisesGrd)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ExportarBtn;
+        private System.Windows.Forms.DataGridView PaisesGrd;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CodigoCol;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NombreCol;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CantidadCol;
+
+    }
+}
---
+++ Views/Paises/FrmListadoPaises.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.IO; //agregada para exportar
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using TurApp.db;
+
+namespace TurApp.Views
+{
+    public partial class FrmListadoPaises : Form
+    {
+        public FrmListadoPaises()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmListadoDestinos_Load(object sender, EventArgs e)
+    {
+        this.PaisesGrd.AutoGenerateColumns = false;
+        var paises = Pais.FindAllStatic(null, (p1, p2) => p1.Codigo.CompareTo(p2.Codigo));
+        var PaisesConTuristas = filtrarPaisesPorDestino(paises);
+        var PaisesBindingList = new BindingList<PaisConTuristas>(PaisesConTuristas);
+        var PaisesBindingSource = new BindingSource(PaisesBindingList, null);
+        this.PaisesGrd.DataSource = PaisesBindingSource;
+
+        this.ExportarBtn.Enabled = true;
+
+        this.PaisesGrd.ColumnHeaderMouseClick += new DataGridViewCellMouseEventHandler(PaisesGrd_ColumnHeaderMouseClick);
+    }
+
+    private class PaisConTuristas
+    {
+        public Pais PaisCol;
+        public int CodigoCol;
+        public int CantidadCol;
+    }
+
+    private List<PaisConTuristas> filtrarPaisesPorDestino(List<Pais> Paises)
+    {
+        var PaisesConTuristas = new List<PaisConTuristas>();
+        var Turistas = Turista.FindAllStatic(null, (p1, p2) => p1.CodPais.CompareTo(p2.CodPais));
+
+        foreach (Pais pais in Paises)
+        {
+            var codPais = pais.Codigo;
+            int count = 0;
+            foreach (Turista turista in Turistas)
+            {
+                if (codPais == turista.CodPais)
+                {
+                    count++;
+                }
+            }
+        PaisesConTuristas.Add(new PaisConTuristas { PaisCol = pais, CodigoCol = codPais, CantidadCol = count });
+    }
+
+        return PaisesConTuristas.OrderBy(p => p.CodigoCol).ToList();
+    }
+
+        private void PaisesGrd_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
+        {
+            this.PaisesGrd.DataBindingComplete -= PaisesGrd_DataBindingComplete;
+
+            try
+            {
+                for (int i = 0; i < this.PaisesGrd.Rows.Count; ++i)
+                {
+                    DataGridViewRow item = this.PaisesGrd.Rows[i];
+                    item.Cells[1].Value = (item.DataBoundItem as Pais).Nombre;
+                }
+            }
+            finally
+            {
+                this.PaisesGrd.DataBindingComplete += PaisesGrd_DataBindingComplete;
+            }
+        }
+
+        private void PaisesGrd_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void ExportarBtn_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog sfd = new SaveFileDialog())
+            {
+                sfd.Filter = "CSV files (*.csv)|*.csv|All files (*.*)|*.*";
+                sfd.FilterIndex = 1;
+                sfd.RestoreDirectory = true;
+
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    ExportToCsv(PaisesGrd, sfd.FileName);
+                }
+            }
+        }
+
+        private void ExportToCsv(DataGridView dgv, string filePath)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(filePath))
+                {
+                    // Escribir encabezados
+                    for (int i = 0; i < dgv.Columns.Count; i++)
+                    {
+                        sw.Write(dgv.Columns[i].HeaderText);
+                        if (i < dgv.Columns.Count - 1)
+                        {
+                            sw.Write(",");
+                        }
+                    }
+                    sw.WriteLine();
+
+                    // Escribir datos
+                    for (int i = 0; i < dgv.Rows.Count; i++)
+                    {
+                        for (int j = 0; j < dgv.Columns.Count; j++)
+                        {
+                            sw.Write(dgv.Rows[i].Cells[j].Value);
+                            if (j < dgv.Columns.Count - 1)
+                            {
+                                sw.Write(",");
+                            }
+                        }
+                        sw.WriteLine();
+                    }
+                }
+                MessageBox.Show("Datos exportados con éxito.", "Exportación completada", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al exportar datos: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void PaisesGrd_ColumnHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+
+        }
+
+        private void FrmListadoPaises_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
