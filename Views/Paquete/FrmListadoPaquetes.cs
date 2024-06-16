1fd403130409ec8bcb55ddd0068c131af6aff5c4
ilugr <bustamante.gg08@gmail.com>
2024-06-15 23:35:07 -0300

    Formularios de Paquete

---
+++ Views/Paquete/FrmListadoPaquetes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using TurApp.db;
+
+namespace TurApp.Views
+{
+    public partial class FrmListadoPaquetes : FormBase
+    {
+        public FrmListadoPaquetes()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmListadoPaquetes_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
